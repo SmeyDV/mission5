675cfe380cfda06a47d2d279f2cba0cd2c154caa
SmeyDeveloper_X <reaksmeyrin15@gmail.com>
2024-08-19 22:41:46 +0700

    change login and signup page

--- loginpage.php
+++ loginpage.php
@@ -60,7 +60,7 @@
         Don't have an account?
         <a href="signuppage.html">Sign up</a>
       </p>
-      <a href="index.html" class="back">Back</a>
+      <a href="index.php" class="back">Back</a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
--- signuppage.php
+++ signuppage.php
@@ -71,7 +71,7 @@
         Already have an account?
         <a href="loginpage.html">Log in</a>
       </p>
-      <a href="index.html" class="back">Back</a>
+      <a href="index.php" class="back">Back</a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
